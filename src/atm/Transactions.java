89e08540559d59009baf434bab12f3a7058e4cb1
zakoyangayane <gayanezakoyan@gmail.com>
2020-03-25 15:50:44 +0400

    added method desriptions

--- src/atm/Transactions.java
+++ src/atm/Transactions.java
@@ -10,11 +10,35 @@ import java.util.Map;
  * @since 25.03.2020
  */
 public interface Transactions {
+
+    /**
+     * transactions history of a current user
+     *
+     * @return the day done the transaction and the action description
+     */
     Map<Date, String> seeTransactionHistory();
 
+    /**
+     * action for taking money on cash from card
+     *
+     * @param amountToTake defining the size of amount
+     * @return the cash from card
+     */
     int withdraw(int amountToTake);
 
+    /**
+     * an action for transferring an amount of money to other user
+     *
+     * @param accountToWhichTransferring the ID of the account holder
+     * @param amountToTransfer           size of transferring money
+     * @return true if the action has been completed successfully
+     */
     boolean transfer(Account accountToWhichTransferring, int amountToTransfer);
 
+    /**
+     * for closing the account from the ATM, it's needed to quit it
+     *
+     * @return true if quited successfully
+     */
     boolean quit();
 }
---
+++ src/service/MyBank.java
@@ -0,0 +1,10 @@
+package service;
+
+import atm.Bank;
+
+public class MyBank {
+
+    //creating a bank
+    Bank myBank = new Bank("Armenia Bank", 15);
+
+}
